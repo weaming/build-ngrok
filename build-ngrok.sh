f31302a16ae26bf8af61f43b223679deab6c1dbb
weaming <garden.yuen@gmail.com>
2017-02-25 06:59:54 +0000

    rebuild

--- build-ngrok.sh
+++ build-ngrok.sh
@@ -9,7 +9,7 @@ dist="$root/../dist"
 git clone https://github.com/inconshreveable/ngrok && cd $root
 #https://github.com/inconshreveable/ngrok/issues/181
 go get github.com/jteeuwen/go-bindata
-[ -d $root/bin ] || mkdir bin && cp $GOPATH/bin/go-bindata $root/bin
+[ -d $root/bin ] || mkdir $root/bin && cp $GOPATH/bin/go-bindata $root/bin
 
 # 自建ngrokd服务，如果不想买SSL证书，我们需要生成自己的自签名证书，并编译一个携带该证书的ngrok客户端
 [ -d $dir ] || mkdir $dir && cd $dir
@@ -33,7 +33,9 @@ make release-client
 # build for my raspberry
 unset GOBIN && GOOS="linux" GOARCH="arm" make release-client
 
+rm -rf $dist
 [ -d $dist ] || mkdir $dist
-cp $root/bin/ngrok* $dist
+#cp $root/bin/ngrok* $dist
+cp $GOPATH/bin/ngrok* $dist
 cp -r $root/bin/*arm* $dist
 
